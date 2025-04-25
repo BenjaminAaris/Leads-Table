63bb47e87e8a949c3e414068400e535ad8ac4121
BenjaminAaris <b_aarisandersen@outlook.dk>
2025-04-25 12:38:21 +0200

    hej

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Leads</title>
+<link rel="stylesheet" href="index.css">
+<body>
+<h1>Leads table</h1> 
+<h2> Here you can see all the information from the DataBase </h2>
+    <table>
+        <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+        </tr>
+        <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+        </tr>
+        <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+        </tr>
+    </table>
+
+
+</body>
+</html>
