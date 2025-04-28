460c37d6e735993633bae7a3b8a2892912676108
BenjaminAaris <b_aarisandersen@outlook.dk>
2025-04-28 11:31:19 +0200

    h
    hej

--- index.php
+++ index.php
@@ -1,40 +1,74 @@
+<?php
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "leads_table";
 
+  $conn = mysqli_connect($servername, $username, $password, "leads_table");
+
+  if ($conn->connect_error) {
+    echo "Connection failed: " . $conn->connect_error;
+  }
+
+
+    
+    $sql = 'select * from leads';
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result)>0) {
+      
+    } else {
+      echo "0 results";
+    }
+
+  mysqli_close($conn);
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Leads</title>
-<link rel="stylesheet" href="index.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="index.css">
+</head>
 <body>
-<h1>Leads table</h1> 
-<h2> Here you can see all the information from the DataBase </h2>
+  <table>
+  <colgroup>
+  <col span="3" >
+</colgroup>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Mail</th>
+      <th>City</th>
+      <th>Postal code</th>
+      <th>Vat number</th>
+      <th>Phone number</th>
+      <th>Website</th>
+      <th>Adress</th>
+    </tr>
 
-
+    <?php 
+    
+    while ($row = mysqli_fetch_assoc($result)){
+      echo "
+      <tr>
+        <td>" . $row['id'] . "</td>
+        <td>" . $row['name'] ."</td>
+        <td>" . $row['mail'] ."</td>
+        <td>" . $row['city'] ."</td>
+        <td>" . $row['postal_code'] ."</td>
+        <td>" . $row['vat_number'] ."</td>
+        <td>" . $row['phone'] ."</td>
+        <td>" . $row['website'] ."</td>
+        <td>" . $row['adress'] ."</td>
+      </tr> 
+      ";
+    }
+    ?>
+    
+  </table>
 </body>
 </html>
 
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-
-$conn = new mysqli($servername, $username, $password);
-
-if ($conn->connect_error) {
-  echo "Connection failed: " . $conn->connect_error;
-}
-  echo "Connected successfully";
-  
-  $sql = "select * from leads";
-
-  $mysql = $conn->query($sql);
-
-  while($row = $result->fetch_assoc()) {
-    echo $row["name"];
-  }
-
-  $conn->close();
-
-?>
